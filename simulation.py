02e4e0f0b8cd432a2bd3583696899b34f3df428a
dingare <30653691+dingare@users.noreply.github.com>
2020-11-06 11:58:35 -0500

    Add files via upload

--- simulation.py
+++ simulation.py
@@ -7,12 +7,38 @@ Created on Fri Nov  6 10:42:36 2020
 import pandas as pd
 import numpy as np
 import os
+import gc
+import datetime as dt
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+
+import matplotlib
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-poster')
+p = matplotlib.rcParams
+p["font.size"] = 20
+p["axes.unicode_minus"] = False
+p['lines.linewidth'] = 3
+p['pdf.fonttype'] = 42
+p['ps.fonttype'] = 42
+p["figure.figsize"] = [12, 8]
+p['grid.color'] = 'k'
+p['grid.linestyle'] = ':'
+p['grid.linewidth'] = 0.5
+cmap = plt.get_cmap('viridis')
+colors = cmap(np.linspace(0, 1, 5))  
+import seaborn as sns
+sns.set(style="whitegrid")
+
 os.chdir(r'C:\Users\38030\OneDrive\CORNELL\2020F\AB')
 #%%
-x1 = np.random.standard_t(8,size=50000)
-x2 = np.random.normal(2,size=50000)
-x3 = np.random.normal(2,size=50000)
+x1 = np.random.normal(0,size=50000)
+x2 = np.random.normal(0,size=50000)
+x3 = np.random.normal(0,size=50000)
 x4 = np.random.standard_t(8,size=50000)
+
 x5 = np.linspace(-3,3,num=50000)
 np.random.shuffle(x5)
 
@@ -27,20 +53,30 @@ def transform1(x):
         return(0)
 
 def transform2(x):
-    if x>1.2 or x<-0.5:
-        return(x**2)
-    elif x<-0.8:
-        return(0)
+    if 5>x>1.2 or x<-1:
+        return(x**2/2)
+    elif x>-2.5:
+        return(x/2-1)
     else:
-        return(2*(-x)-0.25)
+        return(0)
         
+def transform45(x):
+    return(int(x.x4>1 and x.x5<-0.7))       
 
 X['x1_transform'] = X.x1.apply(transform1)
 X['x2_transform'] = X.x2.apply(transform2)
-X['x12_transform'] = X.x1*X.x2
+X['x45_transform'] = X.apply(transform45,axis=1)
 X['x3_transform'] = 0.5*(X.x3-0.5)**2 #sinx function
 X['x4_transform'] = np.sin(X.x4)
 
-X['y'] = X['x1_transform']*5 + X['x2_transform'] + X['x12_transform']*0.2 + X['x3_transform']*2 \
-         + X['x4_transform']*X['x3_transform']*0.7 -X['x4']*0.8 + X['residual']*0.25
+X['y'] = X['x1_transform']*5 + X['x3_transform'] - X['x45_transform']*3\
+         + X['x2_transform']*2 - X['x4_transform'] + X['residual']
+         
 X.to_csv('simulation_data.csv',index=False)
+
+plt.scatter(X['x1'],X['y'])
+plt.scatter(X['x1'],X['x1_transform']*5)
+plt.show()
+
+
+
